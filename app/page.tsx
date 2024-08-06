6f84ff7d791ffca75660a5318c2aa595925f1145
Noah Paige <noahlandonpaige@gmail.com>
2024-08-05 18:36:05 -0700

    created using 'npx create-next-app@latest'

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <p>hey, it's noah's website.</p>
+      </div>
+    </main>
+  );
+}
---
+++ app/test/page.js
@@ -0,0 +1,9 @@
+export default function testpage() {
+  return (
+    <main>
+      <div>
+        <p>hey, it's noah's testpage.</p>
+      </div>
+    </main>
+  );
+}
