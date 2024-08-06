adbe8ddd9495228d9749462b8dc0c2fcb842aa10
Noah Paige <noahlandonpaige@gmail.com>
2024-08-05 19:32:09 -0700

    fiddling around

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import styled from "styled-components";
+
+const MyHeader = styled.h1`
+  font-size: 1.5rem;
+  text-align: center;
+  color: var(--text-primary);
+`;
+
+const MyBody = styled.p`
+  font-size: 1rem;
+  text-align: center;
+  color: var(--text-secondary);
+`;
+
 export default function Home() {
   return (
     <main>
       <div>
-        <p>hey, it's noah's website.</p>
+        <MyHeader>hey, it's noah's website.</MyHeader>
+        <MyBody>how's it going?</MyBody>
       </div>
     </main>
   );
