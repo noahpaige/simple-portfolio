119cc42ace1775f2391a97e4849bd361bf562b48
Noah Paige <noahlandonpaige@gmail.com>
2024-08-08 13:02:41 -0700

    tweak wording

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <main>
       <div>
         <MyHeader>hey, it's noah's website.</MyHeader>
-        <MyBody>how's it going?</MyBody>
+        <MyBody>work in progress</MyBody>
       </div>
     </main>
   );
