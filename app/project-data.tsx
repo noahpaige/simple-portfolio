0a9f993d022354d1e217d49a98faf8a67af4f836
Noah Paige <noahlandonpaige@gmail.com>
2024-08-13 19:17:02 -0700

    Create project-data.tsx

---
+++ app/project-data.tsx
@@ -0,0 +1,27 @@
+export const Projects = [
+  {
+    title: "Create an Infinite Energy Source",
+    image:
+      "https://unsplash.com/photos/a-person-riding-a-surfboard-on-a-wave-in-the-ocean-zHE3Jq5ZNV0",
+    content: "markdown goes here",
+  },
+  {
+    title: "Solve World Hunger",
+    image: "https://unsplash.com/photos/tunnel-wave-9tta3btd8hE",
+    content: `
+# Hello, world!
+
+This is a Markdown paragraph.
+
+- Item 1
+- Item 2
+- Item 3
+`,
+  },
+  {
+    title: "My 'Do not Develop My App' App",
+    image:
+      "https://unsplash.com/photos/person-surfing-using-white-surfboard-during-daytime-HDWJiaYPK5c",
+    content: "markdown goes here",
+  },
+];
