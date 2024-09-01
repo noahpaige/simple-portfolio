1d299109c6631004942c4deea83f3110df5c8a32
Noah Paige <noahlandonpaige@gmail.com>
2024-08-31 17:00:27 -0700

    Update project-data.tsx

--- app/project-data.tsx
+++ app/project-data.tsx
@@ -1,12 +1,14 @@
 export const projectData = [
   {
     title: "Create an Infinite Energy Source",
+    tags: ["JS", "Web", "3D"],
     image:
       "https://plus.unsplash.com/premium_photo-1663954864472-ee5b9b4c345a?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: "markdown goes here",
   },
   {
     title: "Solve World Hunger",
+    tags: ["JS", "Web", "3D"],
     image:
       "https://images.unsplash.com/photo-1461730278450-19cc1863601c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: `
@@ -21,18 +23,21 @@ This is a Markdown paragraph.
   },
   {
     title: "My 'Do not Develop My App' App",
+    tags: ["JS", "Web", "3D"],
     image:
       "https://images.unsplash.com/photo-1595675024853-0f3ec9098ac7?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: "markdown goes here",
   },
   {
     title: "Build a Time Machine",
+    tags: ["JS", "Web", "3D"],
     image:
       "https://images.unsplash.com/photo-1530981279185-9f0960715267?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: "markdown goes here",
   },
   {
     title: "Create a Virtual Reality World",
+    tags: ["JS", "Web", "3D"],
     image:
       "https://plus.unsplash.com/premium_photo-1682124752476-40db22034a58?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: `
@@ -47,12 +52,14 @@ This is a Markdown paragraph.
   },
   {
     title: "Develop an AI Assistant",
+    tags: ["JS", "Web", "3D"],
     image:
       "https://images.unsplash.com/photo-1599272771314-f3ec16bda3f2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: "markdown goes here",
   },
   {
     title: "Design a Sustainable City",
+    tags: ["JS", "Web", "3D"],
     image:
       "https://images.unsplash.com/photo-1556983852-43bf21186b2a?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     content: `
